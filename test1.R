cbf51cb1fc5b2031beec2b398c87312125d77e88
Sergeometrics~ <sergioalejobta@gmail.com>
2026-09-04 19:26:33 -0500

    arch-test

--- test1.R
+++ test1.R
@@ -1,5 +1,14 @@
 #test 1
+carros<- mtcars
 head(mtcars)
 names(mtcars)
 mtcars$mpg
 plot(mtcars$mpg,mtcars$cyl)
+library(esquisse)
+esquisser()
+ggplot(carros) +
+  aes(x = cyl, y = hp, colour = vs, size = mpg, group = am) +
+  geom_point() +
+  scale_color_gradient() +
+  theme_minimal()
+library(plotly)
