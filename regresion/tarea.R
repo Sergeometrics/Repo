7dc45ecd841d35b9a42ed3c92e898a9d2836635c
Sergeometrics <sergioalejobta@gmail.com>
2026-09-16 20:40:15 -0500

    NO, ya, ahora SI la tarea esta bien xd

--- regresion/tarea.R
+++ regresion/tarea.R
@@ -1,3 +1,4 @@
+#Espacios en blanco porque la pantalla de mi laptop esta dañada en esta parte JAJAJAJ
 
 
 
@@ -7,46 +8,54 @@
 
 
 
-
+#Datos 
 mazda<-readxl::read_xlsx(path = "regresion/mazda2021.xlsx")
 
+
+#Modelo y varianza poblacional 
 model<- lm(data= mazda, precio ~ km)
-summary(model)
+summary(model) 
 
-model$residuals
- 
-var_model <- var(model$residuals)
-
+ #varianza poblacional (just flexing atp)
 sigmasqrd_est<-sum(model$residuals^2)/(length(model$residuals))
 
-var_model-sigmasqrd_est
 
-n <- 100
 
 # Varianza true
 sigma2_true <- sum(model$residuals^2) / (length(model$residuals) - 2)
 
-set.seed(2026)
 
-diffs <- numeric(1000)
+
+diffs <- numeric(1000) #iniciar variables o sino R llora (Y yo también)
+var_sesgada <- numeric(1000)
+sigma2_insesgada <- numeric(1000)
 
 for (i in 1:1000) {
   set.seed(i)
-  idx <- sample(seq_len(nrow(mazda)), size = n, replace = FALSE)
+  idx <- sample(seq_len(nrow(mazda)), size = 100, replace = FALSE)
   muestra <- mazda[idx, c("precio", "km")]
 
   modelo_sim <- lm(precio ~ km, data = muestra)
 
   # Estimador sesgado:
-  var_sesgada <- sum(residuals(modelo_sim)^2) / n
+  var_sesgada[i] <- sum(residuals(modelo_sim)^2) / 100
+
+
+# Varianza de la muestra insesgada 
+sigma2_insesgada[i] <- sum(modelo_sim$residuals^2) / (length(modelo_sim$residuals) - 2)
+
 
   # Guardar la diferencia entre estimador sesgado y  true
-  diffs[i] <- var_sesgada - sigma2_true
+  diffs[i] <- var_sesgada[i] - sigma2_insesgada[i]
+
 }
 
-sesgo <- mean(diffs)
+sesgo_prom <- mean(diffs)
 
 # Sesgo teorico para comparación 
-sesgo_teorico <- - (2 / n) * sigma2_true
+sesgo_teorico <- - (2 / 100) * sigma2_true
 diffs
-c(sesgo= sesgo , "sesgo teorico"= sesgo_teorico)
+c(sesgo= sesgo_prom , "sesgo teorico"= sesgo_teorico)
+
+#  pa ver si si se parecen :3 
+sesgo_prom / sesgo_teorico 
